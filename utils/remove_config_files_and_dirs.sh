d2f77732b740a39350340cf01784906960b2e299
kyberdrb <kyberdrb@gmail.com>
2020-10-12 21:24:34 +0200

    Enable all CLion versions

--- utils/remove_config_files_and_dirs.sh
+++ utils/remove_config_files_and_dirs.sh
@@ -5,5 +5,5 @@ rm -rf ~/.java/.userPrefs/jetbrains/
 rm -rf ~/.config/JetBrains/CLion*/eval/
 
 # consider removing only the line from with property name '.*evl.*': https://stackoverflow.com/questions/11641780/bash-delete-a-line-from-a-file-permanently/11641809#11641809
-rm ~/.config/JetBrains/CLion2020.1/options/other.xml
+rm ~/.config/JetBrains/CLion*/options/other.xml
 
