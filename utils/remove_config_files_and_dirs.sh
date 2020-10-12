253ff0b3075482cd36e75b28734bfcbf42b596b5
kyberdrb <kyberdrb@gmail.com>
2020-10-12 21:22:26 +0200

    Freedom to code

--- utils/remove_config_files_and_dirs.sh
+++ utils/remove_config_files_and_dirs.sh
@@ -3,5 +3,7 @@
 rm -rf ~/.java/.userPrefs/prefs.xml
 rm -rf ~/.java/.userPrefs/jetbrains/
 rm -rf ~/.config/JetBrains/CLion*/eval/
+
+# consider removing only the line from with property name '.*evl.*': https://stackoverflow.com/questions/11641780/bash-delete-a-line-from-a-file-permanently/11641809#11641809
 rm ~/.config/JetBrains/CLion2020.1/options/other.xml
 
