00e9b3193875ca281af415497f43879a6d0fc218
kyberdrb <kyberdrb@gmail.com>
2021-03-04 08:03:12 +0100

    Create symlink in home dir for more convenient launching...
    ...but change the dot '.' to  for launching script via symlink from different locations, e.g. from the home dir

--- reset_clion_evaluation.sh
+++ reset_clion_evaluation.sh
@@ -6,3 +6,17 @@ clear -x
 ./utils/delete_config_files_and_dirs.sh
 ./utils/restore_clion_configuration.sh
 
+full_script_path
+full_script_path="$(readlink --canonicalize "$0")"
+  
+local script_name
+script_name="$(basename "$0")"
+  
+ln -sf "$full_script_path" "$HOME/$script_name"
+
+echo "========================================="
+echo "A shortcut had been placed in your home directory"
+echo "for more convenient launching"
+echo "located at"
+echo "    $HOME/$script_name"
+
