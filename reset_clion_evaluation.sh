9f17325b0abd77ca415fe76d5939dfbda043c526
kyberdrb <kyberdrb@gmail.com>
2021-03-15 20:59:26 +0100

    Enable launching from locations other than script dir

--- reset_clion_evaluation.sh
+++ reset_clion_evaluation.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-clear -x
+main() {
+  clear -x
 
-./utils/backup_clion_configuration.sh
-./utils/delete_config_files_and_dirs.sh
-./utils/restore_clion_configuration.sh
+  SCRIPT_DIR="$(dirname "$(readlink --canonicalize "$0")")"
 
-full_script_path
-full_script_path="$(readlink --canonicalize "$0")"
-  
-local script_name
-script_name="$(basename "$0")"
-  
-ln -sf "$full_script_path" "$HOME/$script_name"
+  "${SCRIPT_DIR}/utils/backup_clion_configuration.sh"
 
-echo "========================================="
-echo "A shortcut had been placed in your home directory"
-echo "for more convenient launching"
-echo "located at"
-echo "    $HOME/$script_name"
+  "${SCRIPT_DIR}/utils/delete_config_files_and_dirs.sh"
 
+  clear -x
+
+  "${SCRIPT_DIR}/utils/restore_clion_configuration.sh"
+
+  local full_script_path
+  full_script_path="$(readlink --canonicalize "$0")"
+    
+  local script_name
+  script_name="$(basename "$0")"
+    
+  ln -sf "$full_script_path" "$HOME/$script_name"
+
+  echo "========================================="
+  echo "A shortcut had been placed in your home directory"
+  echo "for more convenient launching"
+  echo "located at"
+  echo "    $HOME/$script_name"
+}
+
+main
